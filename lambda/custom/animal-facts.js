55f507e4ddb904dd181f255030a9a49b87a9d608
Zachary <zach.owen50@gmail.com>
2018-09-09 22:32:28 -0700

    Added 10 crazy animal facts to the skill.

--- lambda/custom/animal-facts.js
+++ lambda/custom/animal-facts.js
@@ -1,10 +1,43 @@
 module.exports = [
     {
-        "fact": "Giraffes have no vocal cords.",
-        "image": "url needed"
+        "fact": "In world war two, German tank drivers would drive over camel dung for good luck.",
+        "image": ""
+    },
+    {
+        "fact": "Camels will spit at other animals as a defense mechanism.",
+        "image": ""
     },
     {
         "fact": "Elephants are the only land mammal that can't jump.",
         "image": "url needed"
-    }
+    },
+    {
+        "fact": "Elephant Shrews are only found in the continent of Africa.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Giraffes do not have vocal cords.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Koala fingerprints are so much like humans, that some have impeded crime scenes.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Penguins can drink sea water.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Prairie dogs kiss each other by locking teeth to see if they recognize each other.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Sea otters hold hands while they sleep so they don't drift apart.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Many believe kangaroos don't actually fart, which isn't true. However they do produce less methane.",
+        "image": "url needed"
+    },
+
 ]
