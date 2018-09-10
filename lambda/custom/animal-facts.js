5673fac6b549f73e96a195e403b5c3d25098da47
Zachary <zach.owen50@gmail.com>
2018-09-09 20:20:40 -0700

    Name of skill is now crazy animal facts. Now imports an array of animal object facts.

---
+++ lambda/custom/animal-facts.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        "fact": "Giraffes have no vocal cords.",
+        "image": "url needed"
+    },
+    {
+        "fact": "Elephants are the only land mammal that can't jump.",
+        "image": "url needed"
+    }
+]
