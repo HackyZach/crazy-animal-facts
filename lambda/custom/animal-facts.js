3ad88048f1ab16402d8f76f84a3d246eb904506b
Zachary <zach.owen50@gmail.com>
2018-09-11 22:23:38 -0700

    Added images to all facts, all size pictures, photo credits, etc. to standard card.

--- lambda/custom/animal-facts.js
+++ lambda/custom/animal-facts.js
@@ -1,43 +1,73 @@
 module.exports = [
     {
         "fact": "In world war two, German tank drivers would drive over camel dung for good luck.",
-        "image": ""
+        "animalName": "Camel and WW2",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/camel-tank-skeeze_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/camel-tank-skeeze_720x480.jpg",
+        "photoCredit": "skeeze"
+
     },
     {
-        "fact": "Camels will spit at other animals as a defense mechanism.",
-        "image": ""
+        "fact": "The fastest two-legged creatures are Ostriches. Their fastest speed is about sixty miles per hour.",
+        "animalName": "Ostrich",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/Ostrich-Bluesnap_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/Ostrich-Bluesnap_720x480.jpg",
+        "photoCredit": "Bluesnap"
     },
     {
         "fact": "Elephants are the only land mammal that can't jump.",
-        "image": "url needed"
+        "animalName": "Elephant",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/elephant-ludi_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/elephant-ludi_720x480.jpg",
+        "photoCredit": "ludi"
     },
     {
         "fact": "Elephant Shrews are only found in the continent of Africa.",
-        "image": "url needed"
+        "animalName": "Elephant Shrew",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/elephant-shrews-Kapa65_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/elephant-shrews-Kapa65_720x480.jpg",
+        "photoCredit": "Kapa65"
     },
     {
-        "fact": "Giraffes do not have vocal cords.",
-        "image": "url needed"
+        "fact": "Giraffes do not possess vocal cords.",
+        "animalName": "Giraffes",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/giraffes-hbieser_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/giraffes-hbieser_720x480.jpg",
+        "photoCredit": "hbieser"
     },
     {
         "fact": "Koala fingerprints are so much like humans, that some have impeded crime scenes.",
-        "image": "url needed"
+        "animalName": "Koala",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/koala-skeeze_2_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/koala-skeeze_720x480.jpg",
+        "photoCredit": "skeeze"
     },
     {
         "fact": "Penguins can drink sea water.",
-        "image": "url needed"
+        "animalName": "Penguin",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/penguins-derdento_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/penguins-derdento_720x480.jpg",
+        "photoCredit": "derdento"
     },
     {
         "fact": "Prairie dogs kiss each other by locking teeth to see if they recognize each other.",
-        "image": "url needed"
+        "animalName": "Prairie Dog",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/prairie-dog-Myriams-Fotos-editorial-use_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/prairie-dog-Myriams-Fotos-editorial-use_720x480.jpg",
+        "photoCredit": "Myriams Fotos"
     },
     {
         "fact": "Sea otters hold hands while they sleep so they don't drift apart.",
-        "image": "url needed"
+        "animalName": "Sea Otter",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/sea-otter-442683_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/sea-otter-442683_720x480.jpg",
+        "photoCredit": "442683"
     },
     {
-        "fact": "Many believe kangaroos don't actually fart, which isn't true. However they do produce less methane.",
-        "image": "url needed"
+        "fact": "Many believe that kangaroos don't actually fart, which isn't true. However their farts do produce less methane.",
+        "animalName": "Kangaroo",
+        "largeImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/kangaroo-LoneWombatMedia_1_1200x800.jpg",
+        "smallImageURL": "https://s3.amazonaws.com/crazy-animal-facts-pics/kangaroo-LoneWombatMedia_720x480.jpg",
+        "photoCredit": "LoneWombatMedia"
     },
-
 ]
